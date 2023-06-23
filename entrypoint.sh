11541e7684c23f897834e8168234cd0d562deb40
lolohunn <133176951+lolohunn@users.noreply.github.com>
2023-06-23 00:11:55 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'aa7140ac-5ab9-4630-a181-d9eded6733ff'}
 
 # 其他Paas保活
 PAAS1_URL=
@@ -17,8 +17,8 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"49b0380a81cb14dc2ff096c04cbfa415","TunnelSecret":"qGbbzO5OALkVQOAt+ojV6PosBzFbXTMVUMmuCu1pS04=","TunnelID":"000133cd-98e0-4830-8855-f407a477840f"}'
+ARGO_DOMAIN=ueaegan.klaggatope.tk
 
 generate_config() {
   cat > config.json << EOF
